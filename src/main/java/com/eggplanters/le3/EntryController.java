1b5e34c95202b2d1d6e996b47bd3aebf5c9f5464
verc <u1benedict425@gmail.com>
2024-09-21 10:39:05 +0800

    removed unnecessary imports

--- src/main/java/com/eggplanters/le3/EntryController.java
+++ src/main/java/com/eggplanters/le3/EntryController.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
-
 import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
--- src/main/java/com/eggplanters/le3/FileHistory.java
+++ src/main/java/com/eggplanters/le3/FileHistory.java
@@ -1,8 +1,6 @@
 package com.eggplanters.le3;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
